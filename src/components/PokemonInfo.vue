e112ba467bec3ac5e4ff29558204034ee79a91df
antoniogavilanes <antonio.gavilanes@alumnos.uneatlantico.es>
2024-10-07 12:22:11 +0200

    Mejoras

--- src/components/PokemonInfo.vue
+++ src/components/PokemonInfo.vue
@@ -3,22 +3,23 @@
     <div class="pokemon-list">
       <div v-for="pokemon in pokemonList" :key="pokemon.name" class="pokemon-card">
         <h2>{{ capitalizeName(pokemon.name) }}</h2>
-        <img v-if="pokemon.sprite" :src="pokemon.sprite" :alt="pokemon.name" />
+        <img v-if="pokemon.sprite" :src="pokemon.sprite" :alt="`Imagen de ${pokemon.name}`" class="pokemon-image" />
 
         <!-- Detalles del Pokémon -->
         <div class="pokemon-details">
-          <p>Altura: {{ pokemon.height }}</p>
-          <p>Peso: {{ pokemon.weight }}</p>
+          <p>Altura: {{ pokemon.height }} dm</p>
+          <p>Peso: {{ pokemon.weight }} hg</p>
           <p>Tipo:
-            <span v-for="type in pokemon.types" :key="type.slot">{{ capitalizeName(type.type.name) }}</span>
+            <span v-for="type in pokemon.types" :key="type.slot" class="pokemon-type">{{ capitalizeName(type.type.name)
+              }}</span>
           </p>
         </div>
       </div>
     </div>
 
     <div class="pagination">
-      <button @click="previousPage" :disabled="!previousPageUrl">Anterior</button>
-      <button @click="nextPage" :disabled="!nextPageUrl">Siguiente</button>
+      <button @click="previousPage" :disabled="!previousPageUrl" class="pagination-button">Anterior</button>
+      <button @click="nextPage" :disabled="!nextPageUrl" class="pagination-button">Siguiente</button>
     </div>
   </div>
 </template>
